211d0f5f6555dd619f754c38045e28b3bd8e5281
201802699 <201802699@scmsdom.local>
2022-05-12 14:30:21 +0200

    Submitting products online data

---
+++ products_online_data.py
@@ -0,0 +1,118 @@
+import dash
+
+from dash import dcc
+from dash import html
+# import pandas as pd
+# import plotly.graph_objs as go
+# from dash.dependencies import Input, Output
+import pyodbc
+import dash_bootstrap_components as dbc
+app = dash.Dash(
+    external_stylesheets = [dbc.themes.BOOTSTRAP]
+)
+def Products():
+    pass
+
+
+conn = pyodbc.connect(
+    "Driver={SQL Server Native Client 11.0};"
+    "Server=L451\EASYTEST;"
+    "Database=AdventureWorksDW2019;"
+    "Trusted_Connection=yes;"
+)
+
+# Create an empty dictionary to store the data
+
+# Stores quantity and date in a dictionary for 2011
+dataFor2011 = {
+    "Date": [],
+    "OrderQuantity": []
+}
+# Stores quantity and date in a dictionary for 2012
+dataFor2012 = {
+    "Date": [],
+    "OrderQuantity": []
+}
+# Stores quantity and date in a dictionary for 2013
+dataFor2013 = {
+    "Date": [],
+    "OrderQuantity": []
+}
+
+
+
+def readData(conn , data,year):
+    cursor = conn.cursor()
+    cursor.execute(f"""
+     SELECT 
+			sum(fis.OrderQuantity) as quantity_per_day,
+			dd.DayNumberOfYear
+		FROM [dbo].[FactResellerSales] FIS
+		JOIN [dbo].[DimProduct] DP ON DP.ProductKey = FIS.ProductKey
+		 JOIN [dbo].[DimDate] DD ON DD.DateKey = FIS.OrderDateKey or DD.DateKey = FIS.DueDateKey or DD.DateKey = FIS.ShipDateKey
+
+		 where  dd.CalendarYear = {year}
+
+		 group by dd.DayNumberOfYear 
+         order by dd.DayNumberOfYear
+    """)
+    for row in cursor:
+            
+            for i in range(len(row)):
+                if(i%2 == 0):
+                    data["OrderQuantity"].append(row[i])
+                else:
+                    data["Date"].append(row[i])
+    
+    return data
+
+
+readData(conn,dataFor2011,2011) 
+readData(conn,dataFor2012,2012)
+readData(conn,dataFor2013,2013)
+
+
+def product_figure():
+    return html.Div(
+        [
+            html.Div(
+                children=[
+                    html.H1("Online Products Order Quantity"),
+                ],
+                className="text-center",
+            ),
+            
+            dcc.Graph(
+                id='order-quantity-graph',
+                figure={
+                    'data': [
+                        {'x': dataFor2011['Date'],
+                        'y': dataFor2011['OrderQuantity'],
+                        'type': 'line',
+                        'name': 'order quantity Per Day for 2011'},
+                        {'x': dataFor2012['Date'],
+                        'y': dataFor2012['OrderQuantity'],
+                        'type': 'line',
+                        'name': 'order quantity Per Day for 2012'},
+                        {'x': dataFor2013['Date'],
+                        'y': dataFor2013['OrderQuantity'],
+                        'type': 'line',
+                        'name': 'order quantity Per Day for 2013'},
+                    ],
+                    'layout': {
+                        'title': 'Product Orders Per day for 2011-2013',
+                        'xaxis': {'title': 'Day'},
+                        'yaxis': {'title': 'Order Quantity'},
+                        
+                    }
+                },
+            )
+        ]
+    )
+
+
+
+if __name__ == '__main__':
+    app.run_server(port=8080,debug=True)
+
+
---
+++ sidebar.py
@@ -0,0 +1,130 @@
+import dash
+import dash_bootstrap_components as dbc
+
+from dash import html
+from dash import dcc
+
+import plotly.express as px
+from dash.dependencies import Input, Output
+import pandas as pd
+import pyodbc
+
+# File imports for other functions
+
+    # the function that contains online data
+from products_online_data import product_figure
+    # the function that contains reseller data
+
+# data source: https://www.kaggle.com/chubak/iranian-students-from-1968-to-2017
+# data owner: Chubak Bidpaa
+df = pd.read_csv('https://raw.githubusercontent.com/Coding-with-Adam/Dash-by-Plotly/master/Bootstrap/Side-Bar/iranian_students.csv')
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+
+# styling the sidebar
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+# padding for the page content
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+sidebar = html.Div(
+    [
+        html.H2("Group 5", className="display-4 font-weight-bold text-center"),
+        html.Hr(),
+        html.P(
+            "DashBoard For Database Project ", className="lead"
+        ),
+        dbc.Nav(
+            [
+                dbc.NavLink("Products", href="/", active="exact"),
+                dbc.NavLink("Sales", href="/Sales", active="exact"),
+                dbc.NavLink("Orders", href="/Orders", active="exact"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+content = html.Div(id="page-content", children=[], style=CONTENT_STYLE)
+
+app.layout = html.Div([
+    dcc.Location(id="url"),
+    sidebar,
+    content
+])
+
+
+@app.callback(
+    Output("page-content", "children"),
+    [Input("url", "pathname")]
+)
+def render_page_content(pathname):
+    if pathname == "/":
+        return[
+        html.Div(
+            className="row",
+            children=[
+                html.Div(
+                    className="col-md-6",
+                    children=[
+                        product_figure()
+                    ]
+                ),
+                html.Div(
+                    className="col-md-6",
+                    children=[
+                        product_figure()
+                    ]
+                ),
+                
+            ]
+        )
+       
+    ]
+        
+    elif pathname == "/Sales":
+        return [
+                html.H1('Grad School in Iran',
+                        style={'textAlign':'center'}),
+                        # Begining of the graph
+                dcc.Graph(id='bargraph',
+                         figure=px.bar(df, barmode='group', x='Years',
+                         y=['Girls Grade School', 'Boys Grade School'])
+                         )
+                         #  End of the graph
+                ]
+    elif pathname == "/Orders":
+        return [
+                html.H1('High School in Iran',
+                        style={'textAlign':'center'}),
+                dcc.Graph(id='bargraph',
+                         figure=px.bar(df, barmode='group', x='Years',
+                         y=['Girls High School', 'Boys High School']))
+                ]
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
+    )
+
+
+if __name__=='__main__':
+    app.run_server(debug=True, port=8000)
